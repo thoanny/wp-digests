56c5d32577bca954a92d453121a634dc8c8c411c
Anthony Destenay <anthony.destenay@gmail.com>
2014-10-02 15:42:42 +0200

    Modification des noms de script et style

--- inc/admin.class.php
+++ inc/admin.class.php
@@ -14,13 +14,13 @@ class Plugin_Name_Admin {
 
 	public function enqueue_styles() {
 
-		wp_enqueue_style( $this->name, plugin_dir_url( __FILE__ ) . 'css/plugin-name-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->name, plugin_dir_url( __FILE__ ) . 'css/admin.css', array(), $this->version, 'all' );
 
 	}
 
 	public function enqueue_scripts() {
 
-		wp_enqueue_script( $this->name, plugin_dir_url( __FILE__ ) . 'js/plugin-name-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->name, plugin_dir_url( __FILE__ ) . 'js/admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
