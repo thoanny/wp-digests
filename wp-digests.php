a288a577016b0814c3cb76d824e7c9e19aecaffa
Anthony Destenay <anthony.destenay@gmail.com>
2014-10-02 14:03:21 +0200

    Ajout des informations sur le plugin

--- wp-digests.php
+++ wp-digests.php
@@ -1,2 +1,14 @@
 <?php
 
+/**
+ * Plugin Name: 	Digests
+ * Plugin URI: 		http://you.an-d.me
+ * Description: 	...
+ * Version: 		0.1
+ * Author: 			Anthony Destenay
+ * Author URI: 		http://you.an-d.me
+ * License: 		GPL-2.0+
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:		wp-digests
+ * Domain Path:		/lang
+ */
